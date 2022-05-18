0e17113cc51e37351864643c8cab6bba540d932d
jagonmes <65248503+jagonmes@users.noreply.github.com>
2022-05-18 19:28:44 +0200

    a

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ function loadCampaign() {
     /*document.getElementById("campaign").style.display = none;*/
     fetch('./pages/campaign.html')
   .then(response=> response.text())
-  .then(text=> document.getElementById('mainContainer').innerHTML = text);
+  .then(text=> document.getElementById('mainContainer').innerHTML = "");
+  console.log(response);
   }
