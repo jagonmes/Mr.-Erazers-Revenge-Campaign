06387e9574d6b8e4882d345486811b9421708bbc
jagonmes <65248503+jagonmes@users.noreply.github.com>
2022-05-18 23:03:49 +0200

    FAQ

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ loadCampaign();
 function loadCampaign() {
     document.getElementById('navCampaign').classList.add("active");
     document.getElementById('navFAQ').classList.remove("active");
-    document.getElementById('navComments').classList.remove("active");
     document.getElementById('navHelp').classList.remove("active");
     fetch('./pages/campaign.html')
   .then(response=> response.text())
@@ -14,25 +13,14 @@ function loadCampaign() {
   function loadFAQ() {
     document.getElementById('navCampaign').classList.remove("active");
     document.getElementById('navFAQ').classList.add("active");
-    document.getElementById('navComments').classList.remove("active");
     document.getElementById('navHelp').classList.remove("active");
     fetch('./pages/FAQ.html')
-  .then(response=> response.text())
-  .then(text=> document.getElementById('mainContainer').innerHTML = text);
-  }
-  function loadComments() {
-    document.getElementById('navCampaign').classList.remove("active");
-    document.getElementById('navFAQ').classList.remove("active");
-    document.getElementById('navComments').classList.add("active");
-    document.getElementById('navHelp').classList.remove("active");
-    fetch('./pages/comments.html')
   .then(response=> response.text())
   .then(text=> document.getElementById('mainContainer').innerHTML = text);
   }
   function loadHelp() {
     document.getElementById('navCampaign').classList.remove("active");
     document.getElementById('navFAQ').classList.remove("active");
-    document.getElementById('navComments').classList.remove("active");
     document.getElementById('navHelp').classList.add("active");
     fetch('./pages/help.html')
   .then(response=> response.text())
