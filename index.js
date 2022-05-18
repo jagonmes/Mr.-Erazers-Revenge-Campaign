88f12548f29f8011ac2d7b146a380961e5daf125
jagonmes <65248503+jagonmes@users.noreply.github.com>
2022-05-18 19:49:14 +0200

    Botones funcionales

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
+loadCampaign();
+
 function loadCampaign() {
-    /*document.getElementById("campaign").style.display = none;*/
+    document.getElementById('navCampaign').classList.add("active");
+    document.getElementById('navFAQ').classList.remove("active");
+    document.getElementById('navComments').classList.remove("active");
+    document.getElementById('navHelp').classList.remove("active");
     fetch('./pages/campaign.html')
   .then(response=> response.text())
-  .then(text=> document.getElementById('mainContainer').innerHTML = "");
-  console.log(response);
+  .then(text=> document.getElementById('mainContainer').innerHTML = text);
+  }
+  function loadFAQ() {
+    document.getElementById('navCampaign').classList.remove("active");
+    document.getElementById('navFAQ').classList.add("active");
+    document.getElementById('navCommets').classList.remove("active");
+    document.getElementById('navHelp').classList.remove("active");
+    fetch('./pages/FAQ.html')
+  .then(response=> response.text())
+  .then(text=> document.getElementById('mainContainer').innerHTML = text);
+  }
+  function loadComments() {
+    document.getElementById('navCampaign').classList.remove("active");
+    document.getElementById('navFAQ').classList.remove("active");
+    document.getElementById('navComments').classList.add("active");
+    document.getElementById('navHelp').classList.remove("active");
+    fetch('./pages/comments.html')
+  .then(response=> response.text())
+  .then(text=> document.getElementById('mainContainer').innerHTML = text);
+  }
+  function loadHelp() {
+    document.getElementById('navCampaign').classList.remove("active");
+    document.getElementById('navFAQ').classList.remove("active");
+    document.getElementById('navComments').classList.remove("active");
+    document.getElementById('navHelp').classList.add("active");
+    fetch('./pages/help.html')
+  .then(response=> response.text())
+  .then(text=> document.getElementById('mainContainer').innerHTML = text);
   }
