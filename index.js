37bb2234c8327eb1c31e729a8e131cb9837f90fa
jagonmes <65248503+jagonmes@users.noreply.github.com>
2022-05-18 19:20:08 +0200

    prueba

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function loadCampaign() {
     /*document.getElementById("campaign").style.display = none;*/
     fetch('./pages/campaign.html')
   .then(response=> response.text())
-  .then(text=> document.getElementById('mainContainer').innerHTML = text);
+  .then(text=> document.getElementById('mainContainer').innerHTML = "");
   }
